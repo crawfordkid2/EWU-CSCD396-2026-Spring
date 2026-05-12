c2fd36c1616170932d5de513350c1abc807c4ccc
crawfordkid2 <114096196+crawfordkid2@users.noreply.github.com>
2026-05-12 21:18:42 +0000

    main and workflow updates

--- Assignment2/Terraform/main.tf
+++ Assignment2/Terraform/main.tf
@@ -35,12 +35,71 @@ resource "azurerm_container_app_environment" "main" {
 }
 
 data "azurerm_container_registry" "acr" {
-  name                = "crawfordacr2026"
-  resource_group_name = var.resource_group_name
+  name                = var.acr_name
+  resource_group_name = "rg-assignment2-crawford"
+}
 
-  depends_on = [
-    azurerm_resource_group.main
-  ]
+resource "azurerm_servicebus_namespace" "main" {
+  name                = var.service_bus_namespace_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_servicebus_queue" "main" {
+  name         = var.service_bus_queue_name
+  namespace_id = azurerm_servicebus_namespace.main.id
+}
+
+resource "azurerm_storage_account" "function_storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "messages" {
+  name                  = "messages"
+  storage_account_name  = azurerm_storage_account.function_storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_service_plan" "function_plan" {
+  name                = "asp-${var.function_app_name}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "main" {
+  name                = var.function_app_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  storage_account_name       = azurerm_storage_account.function_storage.name
+  storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
+  service_plan_id            = azurerm_service_plan.function_plan.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    application_stack {
+      dotnet_version              = "8.0"
+      use_dotnet_isolated_runtime = true
+    }
+  }
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"        = "dotnet-isolated"
+    "ServiceBus__fullyQualifiedNamespace" = "${azurerm_servicebus_namespace.main.name}.servicebus.windows.net"
+    "ServiceBusQueueName"             = azurerm_servicebus_queue.main.name
+    "StorageAccountName"              = azurerm_storage_account.function_storage.name
+    "StorageContainerName"            = azurerm_storage_container.messages.name
+  }
 }
 
 resource "azurerm_container_app" "main" {
@@ -64,6 +123,16 @@ resource "azurerm_container_app" "main" {
       image  = var.container_image_name
       cpu    = 0.25
       memory = "0.5Gi"
+
+      env {
+        name  = "ServiceBus__fullyQualifiedNamespace"
+        value = "${azurerm_servicebus_namespace.main.name}.servicebus.windows.net"
+      }
+
+      env {
+        name  = "ServiceBusQueueName"
+        value = azurerm_servicebus_queue.main.name
+      }
     }
 
     min_replicas = 1
@@ -83,7 +152,45 @@ resource "azurerm_container_app" "main" {
   }
 }
 
+resource "azurerm_role_assignment" "container_app_servicebus_sender" {
+  scope                = azurerm_servicebus_namespace.main.id
+  role_definition_name = "Azure Service Bus Data Sender"
+  principal_id         = azurerm_container_app.main.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "function_servicebus_receiver" {
+  scope                = azurerm_servicebus_namespace.main.id
+  role_definition_name = "Azure Service Bus Data Receiver"
+  principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "function_storage_blob_contributor" {
+  scope                = azurerm_storage_account.function_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
+}
+
 output "container_app_url" {
   description = "URL of the Container App"
   value       = "https://${azurerm_container_app.main.latest_revision_fqdn}"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "service_bus_namespace" {
+  value = azurerm_servicebus_namespace.main.name
+}
+
+output "service_bus_queue" {
+  value = azurerm_servicebus_queue.main.name
+}
+
+output "function_app_name" {
+  value = azurerm_linux_function_app.main.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.function_storage.name
 }
