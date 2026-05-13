3145fbcc2e6f388c04002d54e73062a3249b64db
crawfordkid2 <114096196+crawfordkid2@users.noreply.github.com>
2026-05-13 01:42:20 +0000

    adding service bus message

--- Assignment2/NetWebBasedApp/Controllers/HomeController.cs
+++ Assignment2/NetWebBasedApp/Controllers/HomeController.cs
@@ -1,14 +1,49 @@
-using System.Diagnostics;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
 using Microsoft.AspNetCore.Mvc;
 using NetWebBasedApp.Models;
+using System.Diagnostics;
 
 namespace NetWebBasedApp.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMessage(string message)
+    {
+        if (string.IsNullOrWhiteSpace(message))
+        {
+            ViewBag.Status = "Please enter a message.";
+            return View("Index");
+        }
+
+        string serviceBusNamespace = _configuration["ServiceBus:fullyQualifiedNamespace"]!;
+        string queueName = _configuration["ServiceBusQueueName"]!;
+
+        await using ServiceBusClient client = new(
+            serviceBusNamespace,
+            new DefaultAzureCredential());
+
+        ServiceBusSender sender = client.CreateSender(queueName);
+
+        await sender.SendMessageAsync(new ServiceBusMessage(message));
+
+        ViewBag.Status = "Message sent to Service Bus!";
+        return View("Index");
     }
 
     public IActionResult Privacy()
@@ -19,6 +54,9 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(new ErrorViewModel
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        });
     }
 }
